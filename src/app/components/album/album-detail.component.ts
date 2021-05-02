635fcafa7fe3879f1be18fbd8818e09ad8cb005e
Francesco <xblasi@hotmail.it>
2021-05-02 17:02:41 +0100

    implemented 'view images of the album'

--- src/app/components/album/album-detail.component.ts
+++ src/app/components/album/album-detail.component.ts
@@ -1,28 +1,58 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { RouterExtensions } from "@nativescript/angular";
+import { ObservableArray } from "@nativescript/core";
+import { UntilDestroy } from "@ngneat/until-destroy";
+import { finalize, take } from "rxjs/operators";
 
-import { Album } from "../../model/album";
+import { Album, Picture } from "../../model/album";
 import { AlbumService } from "../../services/album.service";
 
 /**
  * Displays the collection of the images in an album.
+ * An image can be deleted 
  */
+@UntilDestroy()
 @Component({
     selector: "ns-details",
     templateUrl: "./album-detail.component.html",
+    styleUrls: ["./album-detail.component.css"]
 })
 export class AlbumDetailComponent implements OnInit {
-    item: Album;
+    /** the album */
+    album: Album;
+    /** busy flag for loading spinner */
+    isBusy = true;
+    /** data items for the ListView */
+    dataItems: ObservableArray<Picture>;
 
     constructor(
         private albumService: AlbumService,
+        private router: RouterExtensions,
         private route: ActivatedRoute
-    ) {}
+    ) {
+        this.dataItems = new ObservableArray<Picture>();
+    }
 
     ngOnInit(): void {
         const id = +this.route.snapshot.params.id;
-        this.albumService.getAlbum(id).subscribe(album => {
-            this.item = album;
-        });
+        this.albumService.getAlbum(id)
+            .pipe(
+                take(1),
+                finalize(() => {
+                    this.isBusy = false;
+                })
+            )
+            .subscribe(album => {
+                this.album = album;
+                this.dataItems.push(album.images);
+            });
+    }
+
+    /**
+     * Go back to the home page.
+     */
+    goBack(): void {
+        this.router.back();
     }
 }
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { RouterExtensions } from "@nativescript/angular";
 import { action, ObservableArray } from "@nativescript/core";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
