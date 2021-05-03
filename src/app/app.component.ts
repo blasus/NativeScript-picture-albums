93e6bbbaaef89e87bc38ae4a25ed6a892b1caf82
Francesco <xblasi@hotmail.it>
2021-05-03 15:56:02 +0100

    set up firebase cloud.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { firebase } from "@nativescript/firebase";
 
 @Component({
     selector: "ns-app",
     templateUrl: "./app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit { 
+
+    ngOnInit(): void {
+        // initialise firebase cloud.
+        firebase.init({
+            // Optionally pass in properties for database, authentication and cloud messaging,
+		    // see their respective docs.
+        })
+        .then(
+            instance => {
+                console.log('firebase.init done!');
+            },
+            error => {
+                console.log(`firebase.init eror: ${error}`);
+            }
+        )
+    }
+}
